aeba924bbe148129edb5a86edcd1b6c2fc5713bc
ercaany <ercaany@gmail.com>
2023-02-25 16:05:26 +0300

    rendering lists & conditional content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,37 +1,42 @@
+import React, { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.65,
+    date: new Date(2021, 3, 21),
+  },
+  {
+    id: "e2",
+    title: "New Video Game",
+    amount: 294.66,
+    date: new Date(2022, 4, 12),
+  },
+  {
+    id: "e3",
+    title: "Food",
+    amount: 294.67,
+    date: new Date(2023, 5, 16),
+  },
+  {
+    id: "e4",
+    title: "Travel",
+    amount: 294.68,
+    date: new Date(2021, 6, 19),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurence",
-      amount: 294.65,
-      date: new Date(2021, 3, 21),
-    },
-    {
-      id: "e2",
-      title: "New Video Game",
-      amount: 294.66,
-      date: new Date(2021, 4, 12),
-    },
-    {
-      id: "e3",
-      title: "Food",
-      amount: 294.67,
-      date: new Date(2021, 5, 16),
-    },
-    {
-      id: "e4",
-      title: "Travel",
-      amount: 294.68,
-      date: new Date(2021, 6, 19),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("Adding expense..");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
